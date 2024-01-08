22fb1b3e25f29ecb8acebe69a3b963c6480ea0c0
Sophie Seifert <sophie.seifert@web.de>
2024-01-08 14:44:26 +0100

    added Bewertung and Hilfreich-Frage

--- src/routes/MenuDetails.tsx
+++ src/routes/MenuDetails.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { getMenuById } from '../data/menu';
 import { CakeIcon } from '@heroicons/react/24/outline';
+import { Button } from 'react-aria-components';
 
 export default function MenuDetails() {
   const params = useParams();
@@ -19,6 +20,9 @@ export default function MenuDetails() {
       </div>
       <div className="flex w-full justify-center">
         <p className="pl-6 pt-10 font-bold"> Zutaten:  {item.ingredients.map(i => <span className="font-normal m-0.5"> {i}, </span> )}</p>
+      </div>
+      <div className="flex w-full justify-center">
+        <div className="flex w-full justify-center"> Bewertung: <Button className="hover:text-yellow-500"> &#x2605; </Button> <Button className="hover:text-yellow-500"> &#x2605; </Button> <Button className="hover:text-yellow-500"> &#x2605; </Button> <Button className="hover:text-yellow-500"> &#x2605; </Button> <Button className="hover:text-yellow-500"> &#x2605; </Button> <span className="mr-4"> </span>   War diese Empfehlung hilfreich? <Button className="ml-1 hover:text-yellow-500"> Ja! </Button> <Button className="ml-1 hover:text-yellow-500"> Nein. </Button> </div>
       </div>
       
     </div>
